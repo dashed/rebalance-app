2a270378280c445930f274119a3b0ac629d7f0ad
Alberto Leal <mailforalberto@gmail.com>
2017-05-31 23:11:41 -0400

    read file

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,33 @@ extern crate csv;
 use std::path::Path;
 use std::fs::File;
 use std::error::Error;
+use std::io::Read;
 
 
 fn main() {
 
-    let path = Path::new("assets/fundaccountdetails.csv");
+    let contents = read_file_to_string("assets/fundaccountdetails.csv");
+
+    println!("Hello, world!");
+}
+
+fn read_file_to_string<T: AsRef<Path>>(path_to_file: T) -> String {
+
+    let path = Path::new(path_to_file.as_ref());
+    let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
     let mut file = match File::open(&path) {
         // The `description` method of `io::Error` returns a string that
         // describes the error
-        Err(why) => {
-            let display = path.display();
-panic!("couldn't open {}: {}", display,
-                                                   why.description())
-        },
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
         Ok(file) => file,
     };
 
-    println!("Hello, world!");
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
+        Ok(_) => contents
+    }
+
 }
