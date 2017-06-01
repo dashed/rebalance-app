a63744c4c8b11be39d8b59d2cb3715298837cc42
Alberto Leal <mailforalberto@gmail.com>
2017-05-31 23:47:54 -0400

    snap

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,99 @@
 extern crate csv;
 
-use std::path::Path;
-use std::fs::File;
-use std::error::Error;
-use std::io::Read;
+// rust imports
+
+use std::collections::HashMap;
+
+struct Percent(f64);
+#[derive(Debug)]
+struct Value(f64);
+
+struct Asset {
+    value: Value,
+    target_allocation_percent: Percent
+}
 
 
 fn main() {
 
-    let contents = read_file_to_string("assets/fundaccountdetails.csv");
+    // let contents = read_file_to_string("assets/fundaccountdetails.csv");
+    // let contents = read_file_to_string("assets/targets.csv");
+    let target_map = create_target_map();
+
+    let portfolio_map = create_portfolio_map(target_map);
 
     println!("Hello, world!");
 }
 
-fn read_file_to_string<T: AsRef<Path>>(path_to_file: T) -> String {
+fn create_target_map() -> HashMap<String, Percent> {
 
-    let path = Path::new(path_to_file.as_ref());
-    let display = path.display();
+    let mut reader = csv::Reader::from_path("assets/targets.csv").unwrap();
 
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let mut file = match File::open(&path) {
-        // The `description` method of `io::Error` returns a string that
-        // describes the error
-        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
-        Ok(file) => file,
-    };
+    let mut target_map = HashMap::new();
 
-    let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
-        Ok(_) => contents
+    for result in reader.records() {
+
+        let record = result.unwrap();
+
+        let asset_name = record.get(0).unwrap().trim().to_string();
+        let allocation: Percent = {
+
+            let column = record.get(1).unwrap().trim();
+
+            let allocation = column.parse::<f64>().unwrap();
+
+            if allocation <= 0.0 {
+                continue;
+            }
+
+            Percent(allocation)
+        };
+
+        target_map.insert(asset_name, allocation);
+
     }
 
+    target_map
+}
+
+fn create_portfolio_map(target_map: HashMap<String, Percent>) -> HashMap<String, Asset> {
+
+    let mut reader = csv::Reader::from_path("assets/fundaccountdetails.csv").unwrap();
+
+    let mut portfolio_map = HashMap::new();
+
+    for result in reader.records() {
+
+        let record = result.unwrap();
+
+        let asset_name = record.get(0).unwrap().trim().to_string();
+
+        let value = {
+            let value: String = record
+                .get(3)
+                .unwrap()
+                .trim()
+                .chars()
+                .skip(1)
+                .collect();
+
+            Value(value.parse::<f64>().unwrap())
+        };
+
+        match target_map.get(&asset_name) {
+            None => {},
+            Some(&Percent(target_allocation_percent)) => {
+
+                let asset = Asset {
+                    value: value,
+                    target_allocation_percent: Percent(target_allocation_percent)
+                };
+
+                portfolio_map.insert(asset_name, asset);
+            }
+        }
+
+    }
+
+    portfolio_map
 }
