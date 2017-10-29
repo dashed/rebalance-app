b459747d3d190e000d26b3ebb2573a77fa4f078c
Alberto Leal <mailforalberto@gmail.com>
2017-10-28 23:45:31 -0400

    debug

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,9 @@ use std::env::args;
 
 // local imports
 
-use rebalance::{Asset, lazy_rebalance, to_string};
+use rebalance::{lazy_rebalance, to_debug_string, to_string, Asset};
 
 fn main() {
-
     let contribution_amount = {
         let mut args = args();
         args.next();
@@ -22,8 +21,10 @@ fn main() {
         first_arg.parse::<f64>().unwrap()
     };
 
-    println!("Contributing: {}\n",
-             format!("{:.*}", 2, contribution_amount));
+    println!(
+        "Contributing: {}\n",
+        format!("{:.*}", 2, contribution_amount)
+    );
 
     let target_map = create_target_map();
 
@@ -31,25 +32,26 @@ fn main() {
 
     let balanced_portfolio = lazy_rebalance(contribution_amount, portfolio);
 
-    println!("{}", to_string(balanced_portfolio));
+    println!("{}", to_string(&balanced_portfolio));
+
+    println!("{}", to_debug_string(&balanced_portfolio));
 }
 
 struct Percent(f64);
 
 fn create_target_map() -> HashMap<String, Percent> {
-
-    let mut reader =
-        csv::ReaderBuilder::new().has_headers(false).from_path("assets/targets.csv").unwrap();
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path("assets/targets.csv")
+        .unwrap();
 
     let mut target_map = HashMap::new();
 
     for result in reader.records() {
-
         let record = result.unwrap();
 
         let asset_name = record.get(0).unwrap().trim().to_string();
         let allocation: Percent = {
-
             let column = record.get(1).unwrap().trim();
 
             let allocation = column.parse::<f64>().unwrap();
@@ -62,14 +64,12 @@ fn create_target_map() -> HashMap<String, Percent> {
         };
 
         target_map.insert(asset_name, allocation);
-
     }
 
     target_map
 }
 
 fn create_portfolio(target_map: HashMap<String, Percent>) -> Vec<Asset> {
-
     let mut reader = csv::ReaderBuilder::new()
         .has_headers(false)
         .from_path("assets/fundaccountdetails.csv")
@@ -78,18 +78,12 @@ fn create_portfolio(target_map: HashMap<String, Percent>) -> Vec<Asset> {
     let mut portfolio_map: HashMap<String, Asset> = HashMap::new();
 
     for result in reader.records() {
-
         let record = result.unwrap();
 
         let asset_name = record.get(0).unwrap().trim().to_string();
 
         let value = {
-            let value: String = record.get(3)
-                .unwrap()
-                .trim()
-                .chars()
-                .skip(1)
-                .collect();
+            let value: String = record.get(3).unwrap().trim().chars().skip(1).collect();
 
             value.parse::<f64>().unwrap()
         };
@@ -97,7 +91,6 @@ fn create_portfolio(target_map: HashMap<String, Percent>) -> Vec<Asset> {
         match target_map.get(&asset_name) {
             None => {}
             Some(&Percent(target_allocation_percent)) => {
-
                 let target_allocation_percent =
                     adjust_target_allocation_percent(target_allocation_percent);
 
@@ -106,11 +99,9 @@ fn create_portfolio(target_map: HashMap<String, Percent>) -> Vec<Asset> {
                 portfolio_map.insert(asset_name, asset);
             }
         }
-
     }
 
     for asset_name in target_map.keys() {
-
         if portfolio_map.contains_key(asset_name) {
             continue;
         }
@@ -127,7 +118,6 @@ fn create_portfolio(target_map: HashMap<String, Percent>) -> Vec<Asset> {
     let mut portfolio = vec![];
 
     for (_asset_name, asset) in portfolio_map {
-
         portfolio.push(asset);
     }
 
@@ -136,7 +126,6 @@ fn create_portfolio(target_map: HashMap<String, Percent>) -> Vec<Asset> {
 
 
 fn adjust_target_allocation_percent(target_allocation_percent: f64) -> f64 {
-
     target_allocation_percent / 100.0
 
     // TODO: remove
@@ -145,5 +134,4 @@ fn adjust_target_allocation_percent(target_allocation_percent: f64) -> f64 {
     // } else {
     //     target_allocation_percent
     // }
-
 }
--- src/rebalance.rs
+++ src/rebalance.rs
@@ -6,9 +6,9 @@ use std::io::Write;
 // 3rd-party imports
 
 use num::BigRational;
-use num::{Zero, One};
+use num::{One, Zero};
 use num::traits::cast::FromPrimitive;
-use num::{ToPrimitive, Signed};
+use num::{Signed, ToPrimitive};
 
 use tabwriter::TabWriter;
 
@@ -25,7 +25,6 @@ pub struct Asset {
 
 impl Asset {
     pub fn new(name: String, target_percent: f64, value: f64) -> Self {
-
         assert!(target_percent <= 1.0);
         assert!(target_percent >= 0.0);
 
@@ -44,7 +43,6 @@ impl Asset {
 }
 
 fn comparator(left: &Asset, right: &Asset) -> Ordering {
-
     if left.deviation < right.deviation {
         return Ordering::Less;
     }
@@ -58,21 +56,22 @@ fn comparator(left: &Asset, right: &Asset) -> Ordering {
 
 
 pub fn lazy_rebalance(amount_to_contribute: f64, mut assets: Vec<Asset>) -> Vec<Asset> {
-
     let amount_to_contribute = BigRational::from_f64(amount_to_contribute).unwrap();
 
-    let portfolio_total: BigRational = assets.iter()
+    let portfolio_total: BigRational = assets
+        .iter()
         .fold(BigRational::zero(), |total, ref asset| total + &asset.value);
 
     let total: BigRational = &portfolio_total + &amount_to_contribute;
 
     for asset in assets.iter_mut() {
-
         let target_value = &total * &asset.target_allocation_percent;
 
         // equivalent to: (value - target_value) / target_value
         // see: https://en.wikipedia.org/wiki/Approximation_error#Formal_Definition
         let deviation = (&asset.value / &target_value) - BigRational::one();
+
+        println!("{}\t{}", asset.name, to_f64(&deviation));
 
         asset.actual_allocation = &asset.value / &portfolio_total;
         asset.target_value = Some(target_value);
@@ -80,7 +79,6 @@ pub fn lazy_rebalance(amount_to_contribute: f64, mut assets: Vec<Asset>) -> Vec<
     }
 
     assets.sort_by(|left, right| {
-
         let result = comparator(left, right);
 
         if amount_to_contribute < BigRational::zero() {
@@ -91,7 +89,6 @@ pub fn lazy_rebalance(amount_to_contribute: f64, mut assets: Vec<Asset>) -> Vec<
     });
 
     let (__k, index_to_stop): (BigRational, usize) = {
-
         // since deviations are approx. errors, original author wanted to 'minimize' approx. errors
         // of assets with 'lowest' approx. errors. in other words, assets with lowest approx. error
         // gets first dibs of the contribution first.
@@ -101,11 +98,11 @@ pub fn lazy_rebalance(amount_to_contribute: f64, mut assets: Vec<Asset>) -> Vec<
         let mut amount_left_to_contribute: BigRational = amount_to_contribute.clone();
         // TODO: wtf is this
         let mut __k: BigRational = BigRational::zero();
-        let mut index_to_stop = 0;
+        let mut last_known_index = 0;
 
         for (index, asset) in assets.iter().enumerate() {
             let asset: &Asset = asset;
-            index_to_stop = index;
+            last_known_index = index;
 
             // TODO: remove
             // if amount_left_to_contribute.abs() <= BigRational::zero() {
@@ -135,22 +132,24 @@ pub fn lazy_rebalance(amount_to_contribute: f64, mut assets: Vec<Asset>) -> Vec<
                 amount_left_to_contribute = amount_left_to_contribute - __t;
                 __k = next_least_deviation;
             } else {
-
+                println!("amount_left_to_contribute: {}", amount_left_to_contribute);
                 __k = __k + (amount_left_to_contribute / &__h);
 
-                index_to_stop += 1;
+
+
+                // index_to_stop += 1;
                 break;
                 // TODO: remove
                 // amount_left_to_contribute = BigRational::zero();
             }
-
         }
+
+        let index_to_stop = last_known_index + 1;
 
         (__k, index_to_stop)
     };
 
     for (index, asset) in assets.iter_mut().enumerate() {
-
         if index >= index_to_stop {
             break;
         }
@@ -171,27 +170,102 @@ pub fn lazy_rebalance(amount_to_contribute: f64, mut assets: Vec<Asset>) -> Vec<
     }
 
     assets
-
 }
 
 fn to_f64(fraction: &BigRational) -> f64 {
-
     let numerator = fraction.numer();
     let denominator = fraction.denom();
 
     numerator.to_f64().unwrap() / denominator.to_f64().unwrap()
 }
 
-pub fn to_string(balanced_portfolio: Vec<Asset>) -> String {
+pub fn to_debug_string(balanced_portfolio: &Vec<Asset>) -> String {
+    let mut buf = "ASSET NAME\tTARGET\tVALUE".to_string();
 
+    for asset in balanced_portfolio {
+        // let delta = match asset.delta {
+        //     Some(ref delta) => delta.clone(),
+        //     None => BigRational::zero(),
+        // };
+
+        let target_allocation_percent = to_f64(&asset.target_allocation_percent);
+
+        let target_allocation_percent = if target_allocation_percent <= 1.0 {
+            target_allocation_percent * 100.0
+        } else {
+            target_allocation_percent
+        };
+
+        // let actual_allocation = to_f64(&asset.actual_allocation);
+
+        // let target_value = &(asset.target_value.clone()).unwrap();
+
+        // let final_portion =
+        //     (&asset.value + &delta) * &asset.target_allocation_percent / target_value;
+
+
+        let line = format!(
+            "{}\t{}%\t{}",
+            asset.name,
+            format_f64(target_allocation_percent, 3),
+            format_f64(to_f64(&asset.value), 2)
+        );
+
+        buf = format!("{}\n{}", buf, line);
+    }
+
+    for asset in balanced_portfolio {
+        // let delta = match asset.delta {
+        //     Some(ref delta) => delta.clone(),
+        //     None => BigRational::zero(),
+        // };
+
+        let target_allocation_percent = to_f64(&asset.target_allocation_percent);
+
+        let target_allocation_percent = if target_allocation_percent <= 1.0 {
+            target_allocation_percent * 100.0
+        } else {
+            target_allocation_percent
+        };
+
+        // let actual_allocation = to_f64(&asset.actual_allocation);
+
+        // let target_value = &(asset.target_value.clone()).unwrap();
+
+        // let final_portion =
+        //     (&asset.value + &delta) * &asset.target_allocation_percent / target_value;
+
+
+        let line = format!(
+            "list.push(new Asset(1, `{}`, {}/100, {}));",
+            asset.name,
+            format_f64(target_allocation_percent, 3),
+            format_f64(to_f64(&asset.value), 2)
+        );
+
+
+
+        buf = format!("{}\n{}", buf, line);
+    }
+
+    let mut tw = TabWriter::new(vec![]);
+
+    tw.write_all(buf.as_bytes()).unwrap();
+    tw.flush().unwrap();
+
+    String::from_utf8(tw.into_inner().unwrap()).unwrap()
+}
+
+pub fn to_string(balanced_portfolio: &Vec<Asset>) -> String {
     let mut buf = "Asset name\tAsset value\tHoldings %\tNew holdings %\tTarget allocation \
                    %\tTarget value\t$ to buy/sell"
         .to_string();
 
-    for asset in balanced_portfolio {
+    let mut total_contribution = 0.0;
 
+    for asset in balanced_portfolio {
         let delta = match asset.delta {
-            Some(delta) => delta,
+            Some(ref delta) => delta.clone(),
             None => BigRational::zero(),
         };
 
@@ -205,22 +279,33 @@ pub fn to_string(balanced_portfolio: Vec<Asset>) -> String {
 
         let actual_allocation = to_f64(&asset.actual_allocation);
 
-        let target_value = &asset.target_value.unwrap();
+        let target_value = &(asset.target_value.clone()).unwrap();
 
-        let final_portion = (&asset.value + &delta) * &asset.target_allocation_percent /
-                            target_value;
+        let final_portion =
+            (&asset.value + &delta) * &asset.target_allocation_percent / target_value;
 
-        let line = format!("{}\t{}\t{}\t{}\t{}\t{}\t{}",
-                           asset.name,
-                           format_f64(to_f64(&asset.value), 2),
-                           format_f64(actual_allocation * 100.00, 3),
-                           format_f64(to_f64(&final_portion) * 100.00, 3),
-                           format_f64(target_allocation_percent, 3),
-                           format_f64(to_f64(&target_value), 2),
-                           format_f64(to_f64(&delta), 2));
+        total_contribution += to_f64(&delta);
+
+        let line = format!(
+            "{}\t{}\t{}\t{}\t{}\t{}\t{}",
+            asset.name,
+            format_f64(to_f64(&asset.value), 2),
+            format_f64(actual_allocation * 100.00, 3),
+            format_f64(to_f64(&final_portion) * 100.00, 3),
+            format_f64(target_allocation_percent, 3),
+            format_f64(to_f64(&target_value), 2),
+            format_f64(to_f64(&delta), 2)
+        );
 
         buf = format!("{}\n{}", buf, line);
     }
+
+    let total_line = format!(
+        "Total\t \t \t \t \t \t{}",
+        format_f64(total_contribution, 2)
+    );
+
+    buf = format!("{}\n{}", buf, total_line);
 
     let mut tw = TabWriter::new(vec![]);
 
